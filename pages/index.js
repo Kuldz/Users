404f47c49daba8394596e57391b3fa05990364ec
Kevin Klemm <Kevin.Klemm@tptlive.ee>
2022-02-25 11:17:25 +0200

    QOL changes to make navigation easier, fixed typo in navbar

--- pages/index.js
+++ pages/index.js
@@ -33,22 +33,9 @@ export default function Home () {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="/componentUsage" className={styles.card}>
+            <h2>ComponentUsage Page &rarr;</h2>
+            <p>This sends you to the ComponentUsage page</p>
           </a>
         </div>
       </main>
