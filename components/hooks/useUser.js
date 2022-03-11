2afe324a5c9e63fc3c5c6bc9baca7096bde1503f
Kuldar Kaasik <kuldar.kaasik@tptlive.ee>
2022-03-11 13:20:14 +0200

    Fix trailing spaces

--- components/hooks/useUser.js
+++ components/hooks/useUser.js
@@ -5,10 +5,10 @@ import { useEffect } from "react"
 export default function useUser () {
   const { data: session, status } = useSession()
   const router = useRouter()
-  // This produces a react-hooks/exhaustive-deps warning because it's not inside the useEffect hook, 
+  // This produces a react-hooks/exhaustive-deps warning because it's not inside the useEffect hook,
   // but putting it into the hook causes it to change into a react-hooks/rules-of-hooks error,
   // so I've just opted to ignore it.
-  
+
   useEffect(() => {
     if (status === "unauthenticated") {
       router.push("/login")
