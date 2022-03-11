c90569ec95ee7ef7e8f61be9748d2bfe15fba1d8
Kevin Klemm <Kevin.Klemm@tptlive.ee>
2022-03-11 13:14:23 +0200

    Add fully functional authentication using NextAuth

---
+++ components/hooks/useUser.js
@@ -0,0 +1,19 @@
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+
+export default function useUser () {
+  const { data: session, status } = useSession()
+  const router = useRouter()
+  // This produces a react-hooks/exhaustive-deps warning because it's not inside the useEffect hook, 
+  // but putting it into the hook causes it to change into a react-hooks/rules-of-hooks error,
+  // so I've just opted to ignore it.
+  
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/login")
+    }
+  }, [status])
+
+  return session?.user
+}
--- components/navbar.js
+++ components/navbar.js
@@ -1,50 +1,50 @@
 import { Menu } from "antd"
-import React from "react"
+import React, { useState } from "react"
 import { MailOutlined, UserOutlined, HomeOutlined } from "@ant-design/icons"
+import { signOut } from "next-auth/react"
+import useUser from "./hooks/useUser"
 
-class NavbarAdmin extends React.Component {
-  state = {
+// This component was transformed from React.Component to the more standardized Default Function
+
+export default function NavbarAdmin (props) {
+  const [state, setState] = useState({
     theme: "dark",
     current: "mail"
-  };
+  })
 
-  handleClick = e => {
+  const user = useUser() // Redirects to login if not logged in
+  if (!user) return null // Avoids flickering
+
+  const handleClick = e => {
     console.log("click ", e)
-    this.setState({ current: e.key })
-  };
-
-  changeTheme = value => {
-    this.setState({
-      theme: value ? "dark" : "light"
-    })
-  };
-
-  render () {
-    return (
-      <>
-        <Menu theme={this.state.theme} onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
-          <Menu.Item key="icon" icon={<HomeOutlined /> }>
-            <a href="/"/>
-          </Menu.Item>
-          <Menu.Item key="mail" icon={<MailOutlined />}>
-            Empty
-          </Menu.Item>
-          <Menu.Item>Manage Schools
-            <a href="/manage/School"/>
-          </Menu.Item>
-          <Menu.Item>Manage Students
-            <a href="/manage/Student"/>
-          </Menu.Item>
-          <Menu.Item>Manage Classes
-            <a href="/manage/Class"/>
-          </Menu.Item>
-          <Menu.Item icon={<UserOutlined />} style={{ marginLeft: "auto" }}>
-            Profile
-          </Menu.Item>
-        </Menu>
-      </>
-    )
+    setState({ current: e.key })
   }
-}
 
-export default NavbarAdmin
+  return (
+    <>
+      <Menu theme={state.theme} onClick={handleClick} selectedKeys={[state.current]} mode="horizontal">
+        <Menu.Item key="home" icon={<HomeOutlined /> }>
+          <a href="/"/>
+        </Menu.Item>
+        <Menu.Item key="empty" icon={<MailOutlined />}>
+          Empty
+        </Menu.Item>
+        <Menu.Item key="schools">Manage Schools
+          <a href="/manage/School"/>
+        </Menu.Item>
+        <Menu.Item key="students">Manage Students
+          <a href="/manage/Student"/>
+        </Menu.Item>
+        <Menu.Item key="classes">Manage Classes
+          <a href="/manage/Class"/>
+        </Menu.Item>
+        <Menu.Item key="profile" icon={<UserOutlined />} style={{ marginLeft: "auto" }}>
+          Profile
+        </Menu.Item>
+        <Menu.Item key="signout" onClick={() => signOut()}>
+          Sign Out
+        </Menu.Item>
+      </Menu>
+    </>
+  )
+}
