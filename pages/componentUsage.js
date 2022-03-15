1060b97386926901362f9e8631d1d751402ade5d
RaitBoach <55572889+RaitBoach@users.noreply.github.com>
2022-03-16 00:12:55 +0200

    Fixed errors regarding new rules

--- pages/componentUsage.js
+++ pages/componentUsage.js
@@ -1,18 +1,16 @@
-import React, { Component } from "react"
+import React from "react"
 import Head from "next/head"
 import Nav from "../components/nav"
 
-class componentUsage extends Component {
-  render () {
-    return (
-      <div>
-        <Head>
-          <title>Component Usage and Testing</title>
-        </Head>
-        <Nav />
-      </div>
-    )
-  }
+function componentUsage () {
+  return (
+    <div>
+      <Head>
+        <title>Component Usage and Testing</title>
+      </Head>
+      <Nav />
+    </div>
+  )
 }
 
 export default componentUsage
