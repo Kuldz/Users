7e643646736047c04cb5c74a6aa12864c7229600
Kuldar Kaasik <kuldar.kaasik@tptlive.ee>
2022-03-18 17:40:36 +0200

    Make school editing functional

--- pages/api/v1/schools/[id]/index.js
+++ pages/api/v1/schools/[id]/index.js
@@ -1,13 +1,29 @@
 import { PrismaClient } from "@prisma/client"
 
-export default async function schoolIDHandler (req, res) {
+export default async function schoolIdHandler (req, res) {
   const {
+    query: { id },
     method
   } = req
 
   const prisma = new PrismaClient()
 
   switch (method) {
+    case "PUT": {
+      for (const key in req.body) {
+        if (req.body[key] === null) { delete req.body[key] }
+      }
+
+      const School = await prisma.school.update({
+        where: {
+          id: parseInt(id)
+        },
+        data: req.body
+      })
+      School ? res.status(200).json({ School }) : res.status(404).json({ error: `Could not find school by ID ${id}` })
+      break
+    }
+
     case "DELETE": {
       await prisma.school.delete({
         where: {
