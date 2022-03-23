456280a7e27ebd473fdd6634623c36181242929b
RaitBoach <55572889+RaitBoach@users.noreply.github.com>
2022-03-23 11:30:12 +0200

    Fixed add edit form style

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,8 @@
 import PropTypes from "prop-types"
 import React from "react"
-import { ConfigProvider } from "antd"
 import "antd/dist/antd.variable.min.css"
 import "../styles/global.css"
 import { SessionProvider } from "next-auth/react"
-
-ConfigProvider.config({
-  theme: {
-    primaryColor: "#2275c9",
-    "background-color": "#12171d"
-  }
-})
 
 export default function MyApp ({
   Component, pageProps: { session, ...pageProps }
