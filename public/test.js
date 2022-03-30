9f67ad2e77465298398c9848438a24c3dbc66b42
Kevin Klemm <Kevin.Klemm@tptlive.ee>
2022-03-30 11:00:15 +0300

    Remove unnecessary file

--- public/test.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-const myPlaintextPassword = 'qwe123';
-
-bcrypt.genSalt(saltRounds, function(err, salt) {
-    bcrypt.hash(myPlaintextPassword, salt, function(err, hashedpassword) {
-        console.log(hashedpassword)
-    });
-});
