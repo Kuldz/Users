d3be7dc7225c78b599f1cc8d730ce3118ed3d390
RaitBoach <55572889+RaitBoach@users.noreply.github.com>
2022-04-09 18:35:01 +0300

    Revert "Add seperate rewrite function for CORS"
    This reverts commit 70a53fa7971b02baf93e9523936c3c63c8b42f38.

--- next.config.js
+++ next.config.js
@@ -9,16 +9,9 @@ module.exports = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          { destination: "https://tptusers.vercel.app/api/v1/login" }
         ]
-      }
-    ]
-  },
-  async rewrites () {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://tptusers.vercel.app/api/v1/login"
       }
     ]
   }
